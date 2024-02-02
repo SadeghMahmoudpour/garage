5f1ebb1192117ecca581133d942190416e06e0fb
sadegh <mahmoudpour.sadegh@gmail.com>
2024-02-02 23:03:19 +0330

    Add floor test

---
+++ tests/FloorTest.php
@@ -0,0 +1,193 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Temperworks\Codechallenge\exceptions\NoCapacityException;
+use Temperworks\Codechallenge\models\memory\Floor;
+use Temperworks\Codechallenge\models\VehicleInterface;
+
+class FloorTest extends TestCase
+{
+    /**
+     * @dataProvider provideTestDataForFloorDoesAcceptVehicle
+     */
+    public function testFloorDoesAcceptVehicle(array $testCase)
+    {
+        $floor = new Floor($testCase["cap"], $testCase["excludeVehicleTypes"]);
+        foreach ($testCase["currentVehicles"] as $currentVehicleSize) {
+            $currentVehicle = $this->createStub(VehicleInterface::class);
+            $currentVehicle->method("getSize")->willReturn($currentVehicleSize);
+            $floor->parkVehicle($currentVehicle);
+        }
+
+        $vehicle = $this->createStub(VehicleInterface::class);
+        $vehicle->method("getType")->willReturn($testCase["vehicleType"]);
+        $vehicle->method("getSize")->willReturn($testCase["vehicleSize"]);
+
+        $result = $floor->doesAcceptVehicle($vehicle);
+        self::assertEquals($testCase["result"], $result);
+    }
+
+    /**
+     * @dataProvider provideTestDataForFloorParkVehicle
+     */
+    public function testFloorParkVehicle(array $testCase)
+    {
+        $floor = new Floor($testCase["cap"], $testCase["excludeVehicleTypes"]);
+        foreach ($testCase["currentVehicles"] as $currentVehicleSize) {
+            $currentVehicle = $this->createStub(VehicleInterface::class);
+            $currentVehicle->method("getSize")->willReturn($currentVehicleSize);
+            $floor->parkVehicle($currentVehicle);
+        }
+
+        $vehicle = $this->createStub(VehicleInterface::class);
+        $vehicle->method("getType")->willReturn($testCase["vehicleType"]);
+        $vehicle->method("getSize")->willReturn($testCase["vehicleSize"]);
+
+        if ($testCase["exception"]) {
+            $this->expectException($testCase["exception"]);
+        } else {
+            $this->expectNotToPerformAssertions();
+        }
+        $floor->parkVehicle($vehicle);
+    }
+
+    public function provideTestDataForFloorDoesAcceptVehicle()
+    {
+        return [
+            [[
+                "cap" => 1,
+                "excludeVehicleTypes" => [],
+                "currentVehicles" => [],
+                "vehicleType" => "v",
+                "vehicleSize" => 1.0,
+                "result" => true,
+            ]],
+            [[
+                "cap" => 1,
+                "excludeVehicleTypes" => [],
+                "currentVehicles" => [],
+                "vehicleType" => "v",
+                "vehicleSize" => 0.5,
+                "result" => true,
+            ]],
+            [[
+                "cap" => 1,
+                "excludeVehicleTypes" => [],
+                "currentVehicles" => [],
+                "vehicleType" => "v",
+                "vehicleSize" => 1.5,
+                "result" => false,
+            ]],
+            [[
+                "cap" => 0,
+                "excludeVehicleTypes" => [],
+                "currentVehicles" => [],
+                "vehicleType" => "v",
+                "vehicleSize" => 1.0,
+                "result" => false,
+            ]],
+            [[
+                "cap" => 1,
+                "excludeVehicleTypes" => [],
+                "currentVehicles" => [0.5],
+                "vehicleType" => "v",
+                "vehicleSize" => 0.5,
+                "result" => true,
+            ]],
+            [[
+                "cap" => 1,
+                "excludeVehicleTypes" => [],
+                "currentVehicles" => [0.5],
+                "vehicleType" => "v",
+                "vehicleSize" => 1.0,
+                "result" => false,
+            ]],
+            [[
+                "cap" => 2,
+                "excludeVehicleTypes" => [],
+                "currentVehicles" => [0.5],
+                "vehicleType" => "v",
+                "vehicleSize" => 1.0,
+                "result" => true,
+            ]],
+            [[
+                "cap" => 1,
+                "excludeVehicleTypes" => ["v"],
+                "currentVehicles" => [],
+                "vehicleType" => "v",
+                "vehicleSize" => 0.5,
+                "result" => false,
+            ]],
+        ];
+    }
+
+    public function provideTestDataForFloorParkVehicle()
+    {
+        return [
+            [[
+                "cap" => 1,
+                "excludeVehicleTypes" => [],
+                "currentVehicles" => [],
+                "vehicleType" => "v",
+                "vehicleSize" => 1.0,
+                "exception" => null,
+            ]],
+            [[
+                "cap" => 1,
+                "excludeVehicleTypes" => [],
+                "currentVehicles" => [],
+                "vehicleType" => "v",
+                "vehicleSize" => 0.5,
+                "exception" => null,
+            ]],
+            [[
+                "cap" => 1,
+                "excludeVehicleTypes" => [],
+                "currentVehicles" => [],
+                "vehicleType" => "v",
+                "vehicleSize" => 1.5,
+                "exception" => NoCapacityException::class,
+            ]],
+            [[
+                "cap" => 0,
+                "excludeVehicleTypes" => [],
+                "currentVehicles" => [],
+                "vehicleType" => "v",
+                "vehicleSize" => 1.0,
+                "exception" => NoCapacityException::class,
+            ]],
+            [[
+                "cap" => 1,
+                "excludeVehicleTypes" => [],
+                "currentVehicles" => [0.5],
+                "vehicleType" => "v",
+                "vehicleSize" => 0.5,
+                "exception" => null,
+            ]],
+            [[
+                "cap" => 1,
+                "excludeVehicleTypes" => [],
+                "currentVehicles" => [0.5],
+                "vehicleType" => "v",
+                "vehicleSize" => 1.0,
+                "exception" => NoCapacityException::class,
+            ]],
+            [[
+                "cap" => 2,
+                "excludeVehicleTypes" => [],
+                "currentVehicles" => [0.5],
+                "vehicleType" => "v",
+                "vehicleSize" => 1.0,
+                "exception" => null,
+            ]],
+            [[
+                "cap" => 1,
+                "excludeVehicleTypes" => ["v"],
+                "currentVehicles" => [],
+                "vehicleType" => "v",
+                "vehicleSize" => 0.5,
+                "exception" => NoCapacityException::class,
+            ]],
+        ];
+    }
+}
